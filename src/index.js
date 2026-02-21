4f02b05cb80cbbbdd2ede6930e66fae4d8deac39
Sephydev <nicolas.bussiere@outlook.fr>
2026-02-21 23:53:45 +0100

    feat(restaurant-page): Add basic files & config

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './styles.css';
+
+class MyDOM {
+  constructor() {
+    this.container = document.querySelector('#container');
+  }
+
+  createDOM() {
+    const title = document.createElement('h1');
+    title.textContent = "Hello Webpack";
+
+    console.log(this.container)
+
+    this.container.appendChild(title);
+  }
+}
+
+const myDOM = new MyDOM();
+myDOM.createDOM();
